e3042df0caba26e8eb1567e38b05cbfa0bddad7f
Una Garcia <ucj.garcia@gmail.com>
2025-02-10 15:34:28 +0100

    got crop yield csvs, but takes too much ram. redoing in csv format to see if postgres csv importing methods work. Also did data cleaning in earlier dataframes instead of sql statement generation

--- database/postgres/create_db.sh
+++ database/postgres/create_db.sh
@@ -5,6 +5,7 @@ DB_NAME="crop_yields_owid"
 DB_USER="postgres"
 DB_PASSWORD="postgres"
 DB_MODEL_FILE="schema.sql"
+DATA_SQL_FILE="data.sql"
 
 echo "about to create db..."
 # Create database
@@ -12,7 +13,10 @@ sudo -u postgres psql -c "CREATE DATABASE $DB_NAME OWNER postgres;"
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO postgres"
 echo "database created."
 
-
-echo "about to create tables from $DB_MODEL_FILE..."
 # Create tables
+echo "about to create tables from $DB_MODEL_FILE..."
 sudo -u postgres psql -d $DB_NAME -f $DB_MODEL_FILE
+
+# Initialize data
+echo "Initializing data..."
+sudo -u postgres psql -d $DB_NAME -f $DATA_SQL_FILE
