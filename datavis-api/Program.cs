ad0de5ce1c3ad40b77a25451c8ffda803d8cd8b3
Una Garcia <ucj.garcia@gmail.com>
2025-02-18 10:53:58 +0100

    Removed automapper mappings

--- datavis-api/Program.cs
+++ datavis-api/Program.cs
@@ -21,7 +21,7 @@ public class Program
 
         builder.Services.AddScoped<ICropRepository, CropRepository>();
 
-        builder.Services.AddAutoMapper(typeof(Program));
+        // builder.Services.AddAutoMapper(typeof(Program));
 
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
