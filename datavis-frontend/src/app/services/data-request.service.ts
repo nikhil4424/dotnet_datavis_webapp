7bdc91770c01aeedd050a035bd39a02679c38cea
Una Garcia <ucj.garcia@gmail.com>
2025-02-24 16:19:54 +0100

    added function for generating cropyield request and respective api call.

--- datavis-frontend/src/app/services/data-request.service.ts
+++ datavis-frontend/src/app/services/data-request.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ICountry } from '../interfaces/icountry';
 import { Observable } from 'rxjs';
+import { ICropYield } from '../interfaces/icrop-yield';
 
 @Injectable({
   providedIn: 'root'
@@ -17,5 +18,23 @@ export class DataRequestService {
   {
     return this.http.get<ICountry[]>(this.ROOT_URL + "/countries");
   }
-  
+
+  public GetCropYieldsByCountriesAndCrop(countryIds: number[], cropId: number): Observable<ICropYield>{
+    return this.http.get<ICropYield>(this.GenerateCropYieldRequestByCountriesAndCrop(countryIds, cropId));
+  }
+
+
+  private GenerateCropYieldRequestByCountriesAndCrop(countryIds: number[], cropId: number): string
+  {
+    var requestUrl: string = this.ROOT_URL + "/cropyields/countries/crop?"
+    
+    for (let countryId of countryIds){
+      requestUrl += "countryIds=" + countryId + "&";
+    }
+
+    requestUrl += "cropId=" + cropId;
+    console.log(requestUrl);
+
+    return requestUrl;
+  }
 }
