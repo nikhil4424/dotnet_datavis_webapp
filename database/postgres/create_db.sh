e113cbf942bea30f24868a5cf49cffdd20a95f71
Una Garcia <ucj.garcia@gmail.com>
2025-03-11 10:31:31 +0100

    removed commented out code

--- database/postgres/create_db.sh
+++ database/postgres/create_db.sh
@@ -12,42 +12,30 @@ CROP_CSV_FILE="/docker-entrypoint-initdb.d/init_resources/crop.csv"
 CROP_YIELD_CSV_FILE="/docker-entrypoint-initdb.d/init_resources/crop_yield.csv"
 CROPYIELDS_VIEW_CSV_FILE="/docker-entrypoint-initdb.d/init_resources/view_yield.sql"
 
-# DB_MODEL_FILE="schema.sql"
-# DATA_SQL_FILE="data.sql"
-
 export PGPASSWORD=$DB_PASSWORD
 
 echo "about to create db..."
 # Create database
-# sudo -u postgres psql -c "CREATE DATABASE IF NOT EXISTS $DB_NAME OWNER postgres;"
-# sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO postgres"
 psql --username=$DB_USER --command="CREATE DATABASE $DB_NAME OWNER postgres;" || true
 psql --username=$DB_USER --command="GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO postgres"
 echo "database created."
 
 # Create tables
 echo "about to create tables from $DB_MODEL_FILE..."
-# sudo -u postgres psql -d $DB_NAME -f $DB_MODEL_FILE
 psql --username=$DB_USER --dbname=$DB_NAME --file=$DB_MODEL_FILE
 echo "tables created"
 
-# Initialize data
 echo "Initializing data..."
-# sudo -u postgres psql -d $DB_NAME -f $DATA_SQL_FILE
 # Initialize country data from csv
-# sudo -u postgres psql -d $DB_NAME -c "\copy country(name) FROM 'country.csv' DELIMITER ',' CSV HEADER;"
 psql --username=$DB_USER --dbname=$DB_NAME --command="\copy country(name) FROM $COUNTRY_CSV_FILE DELIMITER ',' CSV HEADER;"
 
 # initialize year table from csv
-# sudo -u postgres psql -d $DB_NAME -c "\copy year(value) FROM 'year.csv' DELIMITER ',' CSV HEADER;"
 psql --username=$DB_USER --dbname=$DB_NAME --command="\copy year(value) FROM $YEAR_CSV_FILE DELIMITER ',' CSV HEADER;"
 
 # Initialize crop data from csv
-# sudo -u postgres psql -d $DB_NAME -c "\copy crop(name) FROM 'crop.csv' DELIMITER ',' CSV HEADER;"
 psql --username=$DB_USER --dbname=$DB_NAME --command="\copy crop(name) FROM $CROP_CSV_FILE DELIMITER ',' CSV HEADER;"
 
 # Initialize crop yield data
-# sudo -u postgres psql -d $DB_NAME -c "\copy crop_yield(country_id, year_id, crop_id, value) FROM 'crop_yield.csv' DELIMITER ',' CSV HEADER;"
 psql --username=$DB_USER --dbname=$DB_NAME --command="\copy crop_yield(country_id, year_id, crop_id, value) FROM $CROP_YIELD_CSV_FILE DELIMITER ',' CSV HEADER;"
 
 # Create crop yield view
